f0870f3a035f2415ff4abd47d00d413d0fa35841
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-08-10 23:40:16 +0600

    footer res

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -136,6 +136,37 @@ const Home = () => {
                 </div>
             </Container>
 
+            <section className="bg-gray-900 text-white">
+                <div
+                    className="max-w-screen-[2520px] mx-auto px-4  py-32   lg:items-center"
+                >
+                    <div className=" text-center">
+                        <h1
+                            className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                        >
+                            Let’s Start With Quick App
+                            
+                        </h1>
+
+                        <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                            tenetur fuga ducimus numquam ea!
+                        </p>
+
+                        <div className="mt-8 flex flex-wrap justify-center gap-4">
+                            <a
+                                className="block w-full rounded border  bg-primary px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                                href="/get-started"
+                            >
+                               Request For Early Access
+                            </a>
+
+                         
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 };
